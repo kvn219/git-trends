85f32054421a7a5b2829141e4a1e3b7b2979b761
Kevin Nguyen <kvn219@nyu.edu>
2018-03-11 09:20:06 -0400

    add description for app

--- cmd/root.go
+++ cmd/root.go
@@ -28,9 +28,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "git-trends",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Search trends on Github.",
+	Long: `A simple CLI app in golang for searching trends in Github. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
