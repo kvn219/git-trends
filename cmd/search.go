b9857ac8fead7b62d0e74f8f828126a7e4e49f82
Kevin Nguyen <kvn219@nyu.edu>
2018-03-13 17:07:37 -0400

    add multiple step through options

--- cmd/search.go
+++ cmd/search.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2018 Kevin Nguyen kvn219@nyu.edu
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +20,8 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"net/http"
+	"time"
 
 	"github.com/google/go-github/github"
 	"github.com/kvn219/git-trends/trends"
@@ -35,8 +36,13 @@ var searchCmd = &cobra.Command{
 	Use:   "search",
 	Short: "Your search query!",
 	Long: `You can search for any thing! For example:
- 	$ git-trends --search python
-	$ git-trends --search golang
+
+	$ git-trends search
+	 ✗ What are you searching for?: python
+
+	$ git-trends user # display user info
+
+	$ git-trends repo # display repo info
 	`,
 	Run: addSearch,
 }
@@ -47,50 +53,43 @@ func init() {
 
 func addSearch(cmd *cobra.Command, args []string) {
 	ctx := context.Background()
-	client := github.NewClient(nil)
-	var results = trends.Results{}
-	validate := func(input string) error {
-		if len(input) < 3 {
-			return errors.New("Username must have more than 3 characters")
+	to := &http.Client{Timeout: 10 * time.Second}
+	client := github.NewClient(to)
+	// Get search query from the command line.
+	searchQuery := func(input string) error {
+		if len(input) < 2 {
+			return errors.New("Search query must have more than 2 characters")
 		}
 		return nil
 	}
+	// Set up prompt for the user.
 	prompt := promptui.Prompt{
 		Label:    "What are you searching for?",
-		Validate: validate,
-		Default:  "python",
+		Validate: searchQuery,
+		Default:  "",
 	}
-
+	// Grab user's query.
 	q, err := prompt.Run()
-
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
 	}
+	// Search repos and sort by stars.
 	opts := &github.SearchOptions{Sort: "stars", Order: "desc"}
 	output, resp, err := client.Search.Repositories(ctx, q, opts)
-	defer resp.Body.Close()
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(2)
 	}
+	defer resp.Body.Close()
+	//  Parse results from the body.
+	var results = trends.Results{}
 	results = parseRepoResults(output, results)
-	// for _, q := range args {
-	// 	opts := &github.SearchOptions{Sort: "stars", Order: "desc"}
-	// 	output, resp, err := client.Search.Repositories(ctx, q, opts)
-	// 	defer resp.Body.Close()
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		os.Exit(2)
-	// 	}
-	// 	results = parseRepoResults(output, results)
-	// }
 	for _, repo := range results.Outputs {
 		// fmt.Printf("%#v\n", repo)
 		fmt.Println(repo.ID, repo.Name)
-		fmt.Println(aGlobalFlag)
 	}
-	fpath := "output.json"
+	// Write results to json file.
+	fpath, err := getFilePath()
 	err = saveRepoResults(fpath, results.Outputs)
 	if err != nil {
 		fmt.Printf("%v", err)
@@ -122,4 +121,28 @@ func saveRepoResults(filename string, results []trends.Record) error {
 		return err
 	}
 	return nil
+}
+
+func getFilePath() (string, error) {
+	// Get output file path.
+	userInput := func(input string) error {
+		if len(input) < 2 {
+			return errors.New("Query must be longer than 2 characters")
+		}
+		return nil
+	}
+	// Set up prompt for the user.
+	outputPrompt := promptui.Prompt{
+		Label:     "Where would you like to save the results?",
+		AllowEdit: true,
+		Validate:  userInput,
+		Default:   "output.json",
+	}
+	// Grab user's query.
+	fpath, err := outputPrompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return "", err
+	}
+	return fpath, nil
 }
